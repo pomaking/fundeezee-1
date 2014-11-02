8fafaa1e5d55722744cabbb44fcba94932e5caf2
Kent Hamilton <money.genius@yahoo.com>
2014-11-01 22:48:54 -0400

    adding fe files
    creating fe repository and adding base files

---
+++ js/angular/app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('fundeezee.mainModule', [
+    'fundeezee.mainModule.services',
+    'fundeezee.mainModule.controllers'
+]);
+
+angular.module('fundeezee.ptaModule', [
+    'fundeezee.ptaModule.services',
+    'fundeezee.ptaModule.controllers'
+]);
+
+angular.module('fundeezee.supplierModule', [
+    'fundeezee.supplierModule.services',
+    'fundeezee.supplierModule.controllers'
+]);
+
+angular.module('fundeezeeApp', [
+    'ui.router',
+	'ui.bootstrap',
+	'fundeezee.mainModule',
+    'fundeezee.ptaModule',
+    'fundeezee.supplierModule'
+
+]).config(['$stateProvider', '$httpProvider', function($stateProvider, $httpProvider) {
+
+        $stateProvider
+            .state('/', {
+                url: "",
+                views: {
+                    "leftnav": {
+                        templateUrl: "/fe/partials/main/leftnav.html"
+                    },
+                    "maincontent": {
+                        templateUrl: "/fe/partials/main/home.html",
+                        controller: "HomeCtrl"
+                    }
+                }
+            })
+			.state('supplies', {
+                url: "/supplies",
+                views: {
+                    "leftnav": {
+                        templateUrl: "/fe/partials/main/leftnav.html"
+                    },
+                    "maincontent": {
+                        templateUrl: "/fe/partials/supplier/buysupplies.html",
+                        controller: "BuySuppliesCtrl"
+                    }
+                }
+            })
+			.state('ptaregistration', {
+                url: "/PTA-Registration",
+                views: {
+                    "leftnav": {
+                        templateUrl: "/fe/partials/main/leftnav.html"
+                    },
+                    "maincontent": {
+                        templateUrl: "/fe/partials/pta/pta-registration.html",
+                        controller: "PTARegistrationCtrl"
+                    }
+                }
+            })
+    }]);
---
+++ js/angular/mainModule/services.js
@@ -0,0 +1,5 @@
+'use strict';
+
+/* Services */
+
+var mainServices = angular.module('fundeezee.mainModule.services', []);
