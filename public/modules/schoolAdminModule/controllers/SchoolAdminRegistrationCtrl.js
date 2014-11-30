65ebcb8907764bb53a398694534a0e395318895d
dwible <ddwible@gmail.com>
2014-11-29 23:41:17 -0500

    have the admin registration working with updated school and state

--- public/modules/schoolAdminModule/controllers/SchoolAdminRegistrationCtrl.js
+++ public/modules/schoolAdminModule/controllers/SchoolAdminRegistrationCtrl.js
@@ -9,12 +9,12 @@ schoolAdminControllersModule.controller('SchoolAdminRegistrationCtrl', ['$scope'
       params: {
         q: val,
         resource_id: '102fd9bd-4737-401b-b88f-5c5b0fab94ec',
-		fields: 'SCHNAM09,LEANM09,MZIP09'
+		fields: 'SCHNAM09,LEANM09,MZIP09,LSTATE09'
       }
     }).then(function(response){
 		var schools = [];
 		for(var i=0;i < response.data.result.records.length;i++){
-			schools.push(response.data.result.records[i].SCHNAM09+' ('+response.data.result.records[i].MZIP09+')');
+			schools.push(response.data.result.records[i].SCHNAM09+' ('+response.data.result.records[i].LSTATE09+')');
 		}
 
       //return response.data.result.records;
@@ -25,7 +25,14 @@ schoolAdminControllersModule.controller('SchoolAdminRegistrationCtrl', ['$scope'
   };
 	
 	$scope.schoolAdminRegistrationSubmit = function (ptaDataObj, schoolAdminRegForm) {
+        var x = ptaDataObj.schoolName.indexOf('(');
+        $scope.schoolState =  ptaDataObj.schoolName.substring(x+1,x+3);
+        $scope.schoolName = ptaDataObj.schoolName.substring(0, x).trim();
+
         ptaDataObj.relationshipTitle = 'schoolAdmin';
+        ptaDataObj.schoolName = $scope.schoolName;
+        ptaDataObj.schoolState = $scope.schoolState;
+
 		$scope.ptaRegistrationJSON = {'FEAccount': ptaDataObj};
 		// send create acct form
 	    ptaAcctService.createAcct(createAcctCallback, $scope.ptaRegistrationJSON);
