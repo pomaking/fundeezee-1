732e935fbbe5bf9f7f5275a4f9ceb8867a9ae552
dwible <ddwible@gmail.com>
2014-12-07 19:55:43 -0500

    have the display for the user's choices

--- public/modules/ptaModule/controllers/PTAMembershipPayCtrl.js
+++ public/modules/ptaModule/controllers/PTAMembershipPayCtrl.js
@@ -7,8 +7,54 @@ ptaControllersModule.controller('PTAMembershipPayCtrl', ['$scope', '$http', '$st
 
     };
 
+    var assembleReview = function(){
+        var review = {};
+        var x = ptaAcctService.getReviewChoice();
+        var membership = x.selectedMembership;
+        review.schoolName = x.schoolName;
+        review.ptaName = x.ptaName;
+        if(membership == 'individual'){
+            review.display = 'Individual';
+            review.mCost  = x.individualCost;
+            review.tCost = x.individualCost + 1;
+        }
+        if(membership == 'family'){
+            review.display = 'Family';
+            review.mCost  = x.familyCost;
+            review.tCost = x.familyCost + 1;
+        }
+        if(membership == 'faculty'){
+            review.display = 'Faculty';
+            review.mCost  = x.facultyCost;
+            review.tCost = x.facultyCost + 1;
+        }
+        if(membership == 'business'){
+            review.display = 'Business';
+            review.mCost  = x.businessCost;
+            review.tCost = x.businessCost + 1;
+        }
+        return review;
 
+    }
 
-    $scope.reviewChoice = ptaAcctService.getReviewChoice();
+    $scope.reviewChoice = assembleReview();
 
 }]);
+
+/*
+ ptaregistrationCtrl.ptaSelectedCostsmethod{
+ "_id": "5484c1ddef11bd600c036d12",
+ "schoolName": "JOHNS CREEK ELEMENTARY",
+ "selectedMembership": "family",
+ "selectedContribution": [],
+ "selectedTextContribution": [],
+ "taxExempt": false,
+ "ptaName": "jcpta",
+ "individual": true,
+ "family": true,
+ "individualCost": 8,
+ "familyCost": 10,
+ "membership": [],
+ "contribution": [],
+ "textContribution": []
+ }*/
